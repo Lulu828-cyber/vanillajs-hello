ec137fc4f6a3664941a16ee4afecec5537be3620
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-01-16 11:47:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randonNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  console.log("Generador de excusas!");
+};
+
+let generateExcuse = () => {
+  let subject = ["Big Foot", "A little bird", "My dragon", "your turtle"];
+  let action = ["ate", "stole", "took", "cooked"];
+  let possession = ["the homework", "my toe", "your lunch", "his shoe"];
+  let where = ["in the street", "on the sidewalk", "at the park"];
+
+  let subjectIndex = 0;
+  let actionIndex = 0;
+  let possessionIndex = 0;
+  let whereIndex = 0;
+
+  return (
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
