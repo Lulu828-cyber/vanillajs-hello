cbfdb829a343979784569e01ce5f3c07473bd98c
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-01-17 08:24:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { bottom } from "@popperjs/core";
 
 window.onload = function() {
   let randonNumber = Math.random() * 10;
   document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Generador de excusas!");
 };
-
 let generateExcuse = () => {
   let subject = ["Big Foot", "A little bird", "My dragon", "your turtle"];
   let action = ["ate", "stole", "took", "cooked"];
   let possession = ["the homework", "my toe", "your lunch", "his shoe"];
   let where = ["in the street", "on the sidewalk", "at the park"];
 
-  let subjectIndex = 0;
-  let actionIndex = 0;
-  let possessionIndex = 0;
-  let whereIndex = 0;
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
 
   return (
     subject[subjectIndex] +
